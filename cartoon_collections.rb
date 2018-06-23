9e90b23da2772c8c3bede996cdc3ba4297cbeebd
Tanner Lancaster <tanner@flatironschool.com>
2018-06-23 16:31:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |x, i|
+    i += 1
+    puts "#{i} #{x}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map do |elem|
+    elem.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.any? { |elem| elem.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese()
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
