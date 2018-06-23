81b4eb6eacc94123a8dbbc37ee129d0f1e01fbe2
Tanner Lancaster <tanner@flatironschool.com>
2018-06-23 16:36:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,6 @@ def long_planeteer_calls(arr)
   arr.any? { |elem| elem.length > 4}
 end
 
-def find_the_cheese()
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
